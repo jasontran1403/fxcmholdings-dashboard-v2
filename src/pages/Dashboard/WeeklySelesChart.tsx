f1ee80302be3b52ff56d0424557588309d96af74
jason <hainguyentran14@gmail.com>
2023-12-22 22:15:11 +0700

    update chart

--- src/pages/Dashboard/WeeklySelesChart.tsx
+++ src/pages/Dashboard/WeeklySelesChart.tsx
@@ -1,75 +1,91 @@
 import { Col, Row } from 'react-bootstrap'
 import { ApexOptions } from 'apexcharts'
 import ReactApexChart from 'react-apexcharts'
+import Axios from "axios";
+import { useState, useEffect } from "react";
+
+const url = "https://seashell-app-bbv6o.ondigitalocean.app";
+// const url = "http://localhost:8080";
 
 // components
 import { CustomCardPortlet } from '@/components'
 
-const weeklyChartOpts: ApexOptions = {
-	series: [
-		{
-			name: 'IB',
-			data: [0, 0, 0, 0, 0, 0, 0],
-		},
-	],
-	chart: {
-		height: 377,
-		type: 'bar',
-		toolbar: {
-			show: false,
-		},
-	},
-	plotOptions: {
-		bar: {
-			columnWidth: '60%',
-		},
-	},
-	stroke: {
-		show: true,
-		width: 2,
-		colors: ['transparent'],
-	},
-	dataLabels: {
-		enabled: false,
-	},
-	colors: ['#3bc0c3', '#1a2942', '#d1d7d973'],
-	xaxis: {
-		categories: [
-			'Sunday',
-			'Monday',
-			'Tuesday',
-			'Wednesday',
-			'Thursday',
-			'Friday',
-			'Saturday',
+const WeeklySelesChart = () => {
+	const storedUserData = localStorage?.getItem("_FXCM_AUTH");
+	const parsedUserData = storedUserData ? JSON.parse(storedUserData) : null;
+	const [username] = useState(parsedUserData?.email || '');
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+		  try {
+			const response = await Axios.get(`${url}/api/user/stat/${username}`);
+			setData(response.data);
+		  } catch (error) {
+			console.error('Error fetching data:', error);
+		  }
+		};
+	
+		fetchData();
+	  }, [username]);
+	
+
+	const weeklyChartOpts: ApexOptions = {
+		series: [
+			{
+				name: 'IB',
+				data: data.map(item => item.amount),
+			},
 		],
-	},
-	yaxis: {
-		title: {
-			text: '$ (thousands)',
-		},
-	},
-	legend: {
-		offsetY: 7,
-	},
-	grid: {
-		padding: {
-			bottom: 20,
-		},
-	},
-	fill: {
-		opacity: 1,
-	},
-	tooltip: {
-		y: {
-			formatter: function (val) {
-				return '$ ' + val + ' thousands'
+		chart: {
+			height: 377,
+			type: 'bar',
+			toolbar: {
+				show: false,
 			},
 		},
-	},
-}
+		plotOptions: {
+			bar: {
+				columnWidth: '60%',
+			},
+		},
+		stroke: {
+			show: true,
+			width: 2,
+			colors: ['transparent'],
+		},
+		dataLabels: {
+			enabled: false,
+		},
+		colors: ['#3bc0c3', '#1a2942', '#d1d7d973'],
+		xaxis: {
+			categories: data.map(item => item.time),
+		},
+		yaxis: {
+			title: {
+				text: 'IB',
+			},
+		},
+		legend: {
+			offsetY: 7,
+		},
+		grid: {
+			padding: {
+				bottom: 20,
+			},
+		},
+		fill: {
+			opacity: 1,
+		},
+		tooltip: {
+			y: {
+				formatter: function (val) {
+					return '$ ' + val
+				},
+			},
+		},
+	}
 
-const WeeklySelesChart = () => {
 	return (
 		<CustomCardPortlet
 			cardTitle="Weekly Sales Report"
